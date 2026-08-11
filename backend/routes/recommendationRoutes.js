44d2d16f82f477f1db0dc4c467ee6094342fc21c
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-08-11 13:46:35 +0545

    Initial imports that might be required

---
+++ backend/routes/recommendationRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import User from "../models/User.js";
+import FoodEntry from "../models/FoodEntry.js";
+import WorkoutSession from "../models/WorkoutSession.js";
+import CardioSession from "../models/CardioSession.js";
+import { requireAuth } from "../middleware/auth.js";
+
+
+
+
+
