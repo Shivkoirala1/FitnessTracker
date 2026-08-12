b30daca98b031f09f4aa3a1d9a24e8dd671ce480
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-08-12 15:15:12 +0545

    Adding react data object manipulation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,3 +5,15 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ToastProvider } from "./context/ToastContext.jsx";
 import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
