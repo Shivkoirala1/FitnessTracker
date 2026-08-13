82b211e0a785165de79563bcbe9ea778a2f29915
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-08-13 12:20:50 +0545

    Importing all important pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Nutrition from "./pages/Nutrition.jsx";
+import Workout from "./pages/Workout.jsx";
+import Cardio from "./pages/Cardio.jsx";
+import Profile from "./pages/Profile.jsx";
+import History from "./pages/History.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+
